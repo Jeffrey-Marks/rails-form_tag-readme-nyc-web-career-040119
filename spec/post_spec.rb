97d56a02ed215fad4565db9cfa46cde96e607dee
JDM Ubuntu VB <JKDMarks@gmail.com>
2019-04-25 14:01:51 -0400

    Done.

---
+++ spec/post_spec.rb
@@ -0,0 +1,14 @@
+# # require_relative './spec_helper.rb'
+# require 'spec_helper'
+#
+# describe 'new post' do
+#   it 'ensures that the form route works with the /new action' do
+#     visit new_post_path
+#     expect(page.status_code).to eq(200)
+#   end
+#
+#   it 'renders HTML in the /new template' do
+#     visit new_post_path
+#     expect(page).to have_content('Post Form')
+#   end
+# end
