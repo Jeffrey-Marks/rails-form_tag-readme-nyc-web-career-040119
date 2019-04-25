7006811d1741774ef42a72bd63ecbb798f541f25
JDM Ubuntu VB <JKDMarks@gmail.com>
2019-04-25 14:08:10 -0400

    Done.

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,14 +1,17 @@
-# # require_relative './spec_helper.rb'
+require_relative './spec_helper.rb'
 # require 'spec_helper'
-#
-# describe 'new post' do
-#   it 'ensures that the form route works with the /new action' do
-#     visit new_post_path
-#     expect(page.status_code).to eq(200)
-#   end
-#
-#   it 'renders HTML in the /new template' do
-#     visit new_post_path
-#     expect(page).to have_content('Post Form')
-#   end
-# end
+# require 'rails_helper'
+require_relative '../config/environment.rb'
+
+
+describe 'new post' do
+  it 'ensures that the form route works with the /new action' do
+    visit new_post_path
+    expect(page.status_code).to eq(200)
+  end
+
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
+end
